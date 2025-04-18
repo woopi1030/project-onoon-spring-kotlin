3ad5b06a7b7d5220f7f20904f3e4970856d11935
woopi1030 <lozenkrantz@gmail.com>
2025-04-18 23:37:00 +0900

    create project

---
+++ src/main/kotlin/woopii/project/onoon/OnoonApplication.kt
@@ -0,0 +1,11 @@
+package woopii.project.onoon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OnoonApplication
+
+fun main(args: Array<String>) {
+	runApplication<OnoonApplication>(*args)
+}
---
+++ src/test/kotlin/woopii/project/onoon/OnoonApplicationTests.kt
@@ -0,0 +1,13 @@
+package woopii.project.onoon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnoonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
